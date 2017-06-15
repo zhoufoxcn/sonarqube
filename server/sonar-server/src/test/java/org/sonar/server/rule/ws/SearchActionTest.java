27d733ad01e47955a6feae21df263c56ed5eac17
Simon Brandhof <simon.brandhof@sonarsource.com>
2017-06-15 11:43:35 +0200

    Complete SearchActionTest

--- server/sonar-server/src/test/java/org/sonar/server/rule/ws/SearchActionTest.java
+++ server/sonar-server/src/test/java/org/sonar/server/rule/ws/SearchActionTest.java
@@ -26,27 +26,35 @@ import java.util.stream.Collectors;
 import org.assertj.core.api.iterable.Extractor;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.sonar.api.config.MapSettings;
 import org.sonar.api.resources.Languages;
-import org.sonar.db.DbClient;
+import org.sonar.api.rule.RuleKey;
+import org.sonar.api.rules.RuleType;
+import org.sonar.api.server.ws.WebService;
+import org.sonar.api.utils.System2;
+import org.sonar.api.utils.internal.AlwaysIncreasingSystem2;
+import org.sonar.core.util.stream.MoreCollectors;
 import org.sonar.db.DbTester;
 import org.sonar.db.organization.OrganizationDto;
 import org.sonar.db.rule.RuleDefinitionDto;
 import org.sonar.db.rule.RuleMetadataDto;
-import org.sonar.server.es.EsClient;
 import org.sonar.server.es.EsTester;
+import org.sonar.server.language.LanguageTesting;
 import org.sonar.server.organization.DefaultOrganizationProvider;
 import org.sonar.server.organization.TestDefaultOrganizationProvider;
 import org.sonar.server.rule.index.RuleIndex;
 import org.sonar.server.rule.index.RuleIndexDefinition;
 import org.sonar.server.rule.index.RuleIndexer;
+import org.sonar.server.tester.UserSessionRule;
 import org.sonar.server.text.MacroInterpreter;
-import org.sonar.server.ws.WsAction;
+import org.sonar.server.ws.TestRequest;
 import org.sonar.server.ws.WsActionTester;
 import org.sonarqube.ws.Rules;
 import org.sonarqube.ws.Rules.Rule;
 import org.sonarqube.ws.Rules.SearchResponse;
 
+import static java.util.Arrays.stream;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.guava.api.Assertions.entry;
 import static org.mockito.Matchers.anyString;
@@ -54,29 +62,32 @@ import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.sonar.db.rule.RuleTesting.setSystemTags;
 import static org.sonar.db.rule.RuleTesting.setTags;
+import static org.sonarqube.ws.client.rule.RulesWsParameters.PARAM_RULE_KEY;
 
 public class SearchActionTest {
 
   @org.junit.Rule
-  public DbTester dbTester = DbTester.create();
+  public UserSessionRule userSessionRule = UserSessionRule.standalone();
   @org.junit.Rule
-  public EsTester esTester = new EsTester(
-    new RuleIndexDefinition(new MapSettings()));
+  public ExpectedException thrown = ExpectedException.none();
 
-  private DbClient dbClient = dbTester.getDbClient();
-  private EsClient esClient = esTester.client();
+  private System2 system2 = new AlwaysIncreasingSystem2();
+  @org.junit.Rule
+  public DbTester db = DbTester.create(system2);
+  @org.junit.Rule
+  public EsTester es = new EsTester(new RuleIndexDefinition(new MapSettings()));
 
-  private DefaultOrganizationProvider defaultOrganizationProvider = TestDefaultOrganizationProvider.from(dbTester);
+  private DefaultOrganizationProvider defaultOrganizationProvider = TestDefaultOrganizationProvider.from(db);
+  private RuleIndex ruleIndex = new RuleIndex(es.client());
+  private RuleIndexer ruleIndexer = new RuleIndexer(es.client(), db.getDbClient());
+  private Languages languages = LanguageTesting.newLanguages("java", "js");
+  private ActiveRuleCompleter activeRuleCompleter = new ActiveRuleCompleter(db.getDbClient(), languages);
+  private RuleWsSupport wsSupport = new RuleWsSupport(db.getDbClient(), userSessionRule, defaultOrganizationProvider);
+  private RuleQueryFactory ruleQueryFactory = new RuleQueryFactory(db.getDbClient(), wsSupport);
   private MacroInterpreter macroInterpreter = mock(MacroInterpreter.class);
-  private Languages languages = new Languages();
-  private RuleMapper mapper = new RuleMapper(languages, macroInterpreter);
-  private RuleQueryFactory ruleQueryFactory = new RuleQueryFactory(dbClient, new RuleWsSupport(dbClient, null, defaultOrganizationProvider));
-  private ActiveRuleCompleter activeRuleCompleter = mock(ActiveRuleCompleter.class);
-  private RuleIndex ruleIndex = new RuleIndex(esClient);
-  private WsAction underTest = new SearchAction(ruleIndex, activeRuleCompleter, ruleQueryFactory, dbClient, mapper);
-  private WsActionTester actionTester = new WsActionTester(underTest);
-
-  private RuleIndexer ruleIndexer = new RuleIndexer(esClient, dbClient);
+  private RuleMapper ruleMapper = new RuleMapper(languages, macroInterpreter);
+  private SearchAction underTest = new SearchAction(ruleIndex, activeRuleCompleter, ruleQueryFactory, db.getDbClient(), ruleMapper);
+  private WsActionTester ws = new WsActionTester(underTest);
 
   @Before
   public void before() {
@@ -84,39 +95,115 @@ public class SearchActionTest {
   }
 
   @Test
-  public void should_find_rule() throws IOException {
-    RuleDefinitionDto rule = insertRuleDefinition();
+  public void test_definition() {
+    assertThat(ws.getDef().isPost()).isFalse();
+    assertThat(ws.getDef().since()).isEqualTo("4.4");
+    assertThat(ws.getDef().isInternal()).isFalse();
+    assertThat(ws.getDef().params()).hasSize(22);
+  }
 
-    doReturn("interpreted").when(macroInterpreter).interpret(anyString());
+  @Test
+  public void return_empty_result() {
+    Rules.SearchResponse response = ws.newRequest()
+      .setParam(WebService.Param.FIELDS, "actives")
+      .executeProtobuf(Rules.SearchResponse.class);
 
-    SearchResponse result = actionTester.newRequest()
-      .executeProtobuf(SearchResponse.class);
-    assertThat(result.getRulesList()).extracting(Rule::getKey).containsExactly(rule.getKey().toString());
+    assertThat(response.getTotal()).isEqualTo(0L);
+    assertThat(response.getP()).isEqualTo(1);
+    assertThat(response.getRulesCount()).isEqualTo(0);
+  }
+
+  @Test
+  public void return_all_rules() {
+    RuleDefinitionDto rule1 = createJavaRule();
+    RuleDefinitionDto rule2 = createJavaRule();
+    index();
+
+    verify(r -> {
+    }, rule1, rule2);
+  }
+
+  @Test
+  public void filter_by_rule_key() {
+    RuleDefinitionDto rule1 = createJavaRule();
+    RuleDefinitionDto rule2 = createJavaRule();
+    index();
+
+    verify(r -> r.setParam(PARAM_RULE_KEY, rule1.getKey().toString()), rule1);
+    verifyNoResults(r -> r.setParam(PARAM_RULE_KEY, "missing"));
+  }
+
+  @Test
+  public void return_all_rule_fields_by_default() {
+    RuleDefinitionDto rule = createJavaRule();
+    index();
+
+    Rules.SearchResponse response = ws.newRequest().executeProtobuf(Rules.SearchResponse.class);
+    Rules.Rule result = response.getRules(0);
+    assertThat(result.getCreatedAt()).isNotEmpty();
+    assertThat(result.getEffortToFixDescription()).isNotEmpty();
+    assertThat(result.getHtmlDesc()).isNotEmpty();
+    assertThat(result.hasIsTemplate()).isTrue();
+    assertThat(result.getLang()).isEqualTo(rule.getLanguage());
+    assertThat(result.getLangName()).isEqualTo(languages.get(rule.getLanguage()).getName());
+    assertThat(result.getName()).isNotEmpty();
+    assertThat(result.getRepo()).isNotEmpty();
+    assertThat(result.getSeverity()).isNotEmpty();
+    assertThat(result.getType().name()).isEqualTo(RuleType.valueOf(rule.getType()).name());
+  }
+
+  @Test
+  public void return_subset_of_fields() {
+    RuleDefinitionDto rule = createJavaRule();
+    index();
+
+    Rules.SearchResponse response = ws.newRequest()
+      .setParam(WebService.Param.FIELDS, "createdAt,langName")
+      .executeProtobuf(Rules.SearchResponse.class);
+    Rules.Rule result = response.getRules(0);
+
+    // mandatory fields
+    assertThat(result.getKey()).isEqualTo(rule.getKey().toString());
+    assertThat(result.getType().getNumber()).isEqualTo(rule.getType());
+
+    // selected fields
+    assertThat(result.getCreatedAt()).isNotEmpty();
+    assertThat(result.getLangName()).isNotEmpty();
+
+    // not returned fields
+    assertThat(result.hasEffortToFixDescription()).isFalse();
+    assertThat(result.hasHtmlDesc()).isFalse();
+    assertThat(result.hasIsTemplate()).isFalse();
+    assertThat(result.hasLang()).isFalse();
+    assertThat(result.hasName()).isFalse();
+    assertThat(result.hasSeverity()).isFalse();
+    assertThat(result.hasRepo()).isFalse();
   }
 
   @Test
   public void should_filter_on_organization_specific_tags() throws IOException {
-    OrganizationDto organization = dbTester.organizations().insert();
-    RuleDefinitionDto rule1 = insertRuleDefinition();
+    OrganizationDto organization = db.organizations().insert();
+    RuleDefinitionDto rule1 = createJavaRule();
     RuleMetadataDto metadata1 = insertMetadata(organization, rule1, setTags("tag1", "tag2"));
-    RuleDefinitionDto rule2 = insertRuleDefinition();
+    RuleDefinitionDto rule2 = createJavaRule();
     RuleMetadataDto metadata2 = insertMetadata(organization, rule2);
+    index();
 
-    SearchResponse result = actionTester.newRequest()
+    Consumer<TestRequest> request = r -> r
       .setParam("f", "repo,name")
       .setParam("tags", metadata1.getTags().stream().collect(Collectors.joining(",")))
-      .setParam("organization", organization.getKey())
-      .executeProtobuf(SearchResponse.class);
-    assertThat(result.getRulesList()).extracting(Rule::getKey).containsExactly(rule1.getKey().toString());
+      .setParam("organization", organization.getKey());
+    verify(request, rule1);
   }
 
   @Test
   public void should_list_tags_in_tags_facet() throws IOException {
-    OrganizationDto organization = dbTester.organizations().insert();
-    RuleDefinitionDto rule = insertRuleDefinition(setSystemTags("tag1", "tag3", "tag5", "tag7", "tag9", "x"));
+    OrganizationDto organization = db.organizations().insert();
+    RuleDefinitionDto rule = db.rules().insert(setSystemTags("tag1", "tag3", "tag5", "tag7", "tag9", "x"));
     RuleMetadataDto metadata = insertMetadata(organization, rule, setTags("tag2", "tag4", "tag6", "tag8", "tagA"));
+    index();
 
-    SearchResponse result = actionTester.newRequest()
+    SearchResponse result = ws.newRequest()
       .setParam("facets", "tags")
       .setParam("organization", organization.getKey())
       .executeProtobuf(SearchResponse.class);
@@ -127,9 +214,10 @@ public class SearchActionTest {
 
   @Test
   public void should_include_selected_matching_tag_in_facet() throws IOException {
-    insertRuleDefinition(setSystemTags("tag1", "tag2", "tag3", "tag4", "tag5", "tag6", "tag7", "tag8", "tag9", "tagA", "x"));
+    RuleDefinitionDto rule = db.rules().insert(setSystemTags("tag1", "tag2", "tag3", "tag4", "tag5", "tag6", "tag7", "tag8", "tag9", "tagA", "x"));
+    index();
 
-    SearchResponse result = actionTester.newRequest()
+    SearchResponse result = ws.newRequest()
       .setParam("facets", "tags")
       .setParam("tags", "x")
       .executeProtobuf(SearchResponse.class);
@@ -138,9 +226,10 @@ public class SearchActionTest {
 
   @Test
   public void should_included_selected_non_matching_tag_in_facet() throws IOException {
-    insertRuleDefinition(setSystemTags("tag1", "tag2", "tag3", "tag4", "tag5", "tag6", "tag7", "tag8", "tag9", "tagA"));
+    RuleDefinitionDto rule = db.rules().insert(setSystemTags("tag1", "tag2", "tag3", "tag4", "tag5", "tag6", "tag7", "tag8", "tag9", "tagA"));
+    index();
 
-    SearchResponse result = actionTester.newRequest()
+    SearchResponse result = ws.newRequest()
       .setParam("facets", "tags")
       .setParam("tags", "x")
       .executeProtobuf(SearchResponse.class);
@@ -149,11 +238,12 @@ public class SearchActionTest {
 
   @Test
   public void should_return_organization_specific_tags() throws IOException {
-    OrganizationDto organization = dbTester.organizations().insert();
-    RuleDefinitionDto rule = insertRuleDefinition();
+    OrganizationDto organization = db.organizations().insert();
+    RuleDefinitionDto rule = createJavaRule();
     RuleMetadataDto metadata = insertMetadata(organization, rule, setTags("tag1", "tag2"));
+    index();
 
-    SearchResponse result = actionTester.newRequest()
+    SearchResponse result = ws.newRequest()
       .setParam("f", "tags")
       .setParam("organization", organization.getKey())
       .executeProtobuf(SearchResponse.class);
@@ -164,8 +254,9 @@ public class SearchActionTest {
   }
 
   @Test
-  public void should_return_specified_fields() throws IOException {
-    RuleDefinitionDto rule = insertRuleDefinition();
+  public void should_return_specified_fields() throws Exception {
+    RuleDefinitionDto rule = createJavaRule();
+    index();
 
     checkField(rule, "repo", Rule::getRepo, rule.getRepositoryKey());
     checkField(rule, "name", Rule::getName, rule.getName());
@@ -177,14 +268,14 @@ public class SearchActionTest {
       r -> r.getSysTags().getSysTagsList().stream().collect(Collectors.joining(",")),
       rule.getSystemTags().stream().collect(Collectors.joining(",")));
     checkField(rule, "lang", Rule::getLang, rule.getLanguage());
-    checkField(rule, "langName", Rule::getLangName, rule.getLanguage());
+    checkField(rule, "langName", Rule::getLangName, languages.get(rule.getLanguage()).getName());
     checkField(rule, "gapDescription", Rule::getGapDescription, rule.getGapDescription());
     // to be continued...
   }
 
   @SafeVarargs
   private final <T> void checkField(RuleDefinitionDto rule, String fieldName, Extractor<Rule, T> responseExtractor, T... expected) throws IOException {
-    SearchResponse result = actionTester.newRequest()
+    SearchResponse result = ws.newRequest()
       .setParam("f", fieldName)
       .executeProtobuf(SearchResponse.class);
     assertThat(result.getRulesList()).extracting(Rule::getKey).containsExactly(rule.getKey().toString());
@@ -192,16 +283,36 @@ public class SearchActionTest {
   }
 
   @SafeVarargs
-  private final RuleDefinitionDto insertRuleDefinition(Consumer<RuleDefinitionDto>... populaters) {
-    RuleDefinitionDto ruleDefinitionDto = dbTester.rules().insert(populaters);
-    ruleIndexer.indexRuleDefinition(ruleDefinitionDto.getKey());
-    return ruleDefinitionDto;
-  }
-
-  @SafeVarargs
   private final RuleMetadataDto insertMetadata(OrganizationDto organization, RuleDefinitionDto rule, Consumer<RuleMetadataDto>... populaters) {
-    RuleMetadataDto metadata = dbTester.rules().insertOrUpdateMetadata(rule, organization, populaters);
+    RuleMetadataDto metadata = db.rules().insertOrUpdateMetadata(rule, organization, populaters);
     ruleIndexer.indexRuleExtension(organization, rule.getKey());
     return metadata;
+  }
+
+  private void verifyNoResults(Consumer<TestRequest> requestPopulator) {
+    verify(requestPopulator);
+  }
+
+  private void verify(Consumer<TestRequest> requestPopulator, RuleDefinitionDto... expectedRules) {
+    TestRequest request = ws.newRequest();
+    requestPopulator.accept(request);
+    Rules.SearchResponse response = request
+      .executeProtobuf(Rules.SearchResponse.class);
+
+    assertThat(response.getP()).isEqualTo(1);
+    assertThat(response.getTotal()).isEqualTo(expectedRules.length);
+    assertThat(response.getRulesCount()).isEqualTo(expectedRules.length);
+    RuleKey[] expectedRuleKeys = stream(expectedRules).map(RuleDefinitionDto::getKey).collect(MoreCollectors.toList()).toArray(new RuleKey[0]);
+    assertThat(response.getRulesList())
+      .extracting(r -> RuleKey.parse(r.getKey()))
+      .containsExactlyInAnyOrder(expectedRuleKeys);
+  }
+
+  private void index() {
+    ruleIndexer.indexOnStartup(ruleIndexer.getIndexTypes());
+  }
+
+  private RuleDefinitionDto createJavaRule() {
+    return db.rules().insert(r -> r.setLanguage("java"));
   }
 }
